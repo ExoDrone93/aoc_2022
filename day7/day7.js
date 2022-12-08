0535c8de482f91d61d4c17278119600acfcb3cc9
Adam Gigacz <adam.gigacz@casrd.net>
2022-12-08 17:04:57 +0100

    day7 refactor

--- day7/day7.js
+++ day7/day7.js
@@ -2,76 +2,88 @@ const { readInstructions } = require('../Utils/readFile.js');
 
 const original = readInstructions('day7/input.txt').map(x => x.split(' '));
 
-// console.log(original);
-
 class Node {
   size = 0;
   parent = null;
   children = [];
   name = null;
-  childSumOfSize = null;
-  constructor(size, parent, children, name, childSumOfSize) {
-    this.size = size;
-    this.parent = parent;
-    this.children = children;
+  constructor(size, parent, children, name) {
     this.name = name;
-    this.childSumOfSize = childSumOfSize
+    this.parent = parent;
+    this.size = size;
+    this.children = children;
   }
 
   addChild(newChild) {
     this.children.push(newChild);
   }
 
-  setSize(newSize) {
-    this.size = newSize;
+  getSize() { //read value
+    return this.size;
   }
 
-  setName() {
-    this.name = this.getName();
+  setSize(size) { //set value or etc.
+    this.size = size;
   }
 
-  getName() {
-    return (this.parent?.getName() ?? '') + this.name;
+  getFullName() { //setted in the constructor
+    return (this.parent?.getFullName() ?? '') + this.name;
   }
 
-  getSize() {
-    const newLocal = this.children.reduce((acc, node) => acc += node.getSize(), 0);
-    return this.size + newLocal;
+  recalculateName() {
+    this.name = this.getFullName();
   }
-}
 
-const createNode = (size, parent, children, name, childSumOfSize) => {
-  return new Node(size, parent, children, name, childSumOfSize)
+  recalculateSize() {
+    this.size = this.getSubFolderSize();
+  }
+
+  getSubFolderSize() {
+    return this.size + this.children.reduce((acc, node) => acc += node.getSubFolderSize(), 0);
+  }
 };
 
-const part1 = (array) => {
+const createNode = (size, parent, children, name) => {
+  return new Node(size, parent, children, name)
+};
+
+const buildTree = (array) => {
   const tree = [];
   let currentNode = null;
   array.forEach(([action, command, name]) => {
     if (command == 'cd' && name !== '..') {
-      const node = createNode(0, currentNode, [], name, 0);
-      currentNode?.children.push(node)
+      const node = createNode(0, currentNode, [], name);
+      currentNode?.addChild(node);
       currentNode = node;
       tree.push(node);
       return;
     }
 
     if (command === 'cd' && name === '..') {
-      currentNode = tree.find(node => node.getName() === currentNode?.parent?.getName());
+      currentNode = tree.find(node => node.getFullName() === currentNode?.parent?.getFullName());
+      return;
     }
 
     if (isNaN(action) === false) {
-      currentNode.size += Number(action)
+      currentNode.setSize(currentNode.getSize() + Number(action));
     }
+
   });
   return tree;
 }
 
-const subFinalTree = part1(original);
-
-subFinalTree.forEach(x => {
-  x.size = x.getSize();
-  x.setName()
+const finalTree = buildTree(original).map(node => {
+  node.recalculateName();
+  node.recalculateSize();
+  return node;
 });
 
-console.log(subFinalTree.filter(x => x.size <= 100000).map(x => x.size).reduce((a, b) => a + b));
+const part1 = finalTree.filter(node => node.getSize() <= 100000).map(node => node.getSize()).reduce((a, b) => a + b);
+
+const part2 = finalTree
+  .filter(node => node.getSize() >= (30000000 - (70000000 - finalTree.find(node => node.getFullName() === '/').getSize())))
+  .map(node => node.getSize())
+  .sort((a, b) => a - b)[0];
+
+console.log('part1: ' + part1);
+console.log('part2: ' + part2);
